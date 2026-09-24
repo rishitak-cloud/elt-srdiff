5d8c47e8ee9b90921fd596352485e8105ba98f3f
rishitak-cloud <rkandpal2303@gmail.com>
2026-09-24 17:26:59 +0530

    n=2,l=12

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class SRDiffELTConfig:
     num_heads: int = 4           
     mlp_dim: int = 1024          
     num_blocks: int = 2          
-    max_loops: int = 9           
+    max_loops: int = 12           
     min_loops: int = 1           
     num_rrdb: int = 1            
 
